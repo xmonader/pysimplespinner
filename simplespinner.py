c6ed5a27a241965b02fec5797a1a70f89bd89dfb
xmonader <xmonader@gmail.com>
2016-04-20 16:10:21 +0200

    init \

---
+++ simplespinner.py
@@ -0,0 +1,30 @@
+import time
+import threading
+from threading import Timer, Thread
+from itertools import cycle
+
+class Spinner:
+    def __init__(self, sequence=['|', '/', '-', '\\']):
+        self.seq=sequence
+        self.c=cycle(sequence)
+        self.shouldstop=False
+        
+    def start(self):
+        while not self.shouldstop:
+            print(next(self.c)+"\b", end='')
+                        
+    def stop(self):
+        self.shouldstop=True
+
+
+if __name__ == "__main__":
+     def heavycomp():
+         sp=Spinner()
+         threading._start_new_thread(sp.start, ())
+         print("please wait...")
+         time.sleep(5)
+         sp.stop()
+         print("we are done")
+    
+
+    
