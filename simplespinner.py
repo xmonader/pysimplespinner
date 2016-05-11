d8a2d190cdb1ebe62d60e5e47c9c2223dc520c7c
xmonader <xmonader@gmail.com>
2016-05-11 10:56:23 +0200

    adding interval feature

--- simplespinner.py
+++ simplespinner.py
@@ -4,27 +4,28 @@ from threading import Timer, Thread
 from itertools import cycle
 
 class Spinner:
-    def __init__(self, sequence=['|', '/', '-', '\\']):
+    def __init__(self, interval=0.7, sequence=['|', '/', '-', '\\']):
         self.seq=sequence
         self.c=cycle(sequence)
         self.shouldstop=False
-        
+        self.interval=interval
+
     def start(self):
         while not self.shouldstop:
-            time.sleep(0.07)
+            time.sleep(self.interval)
             print("\r"+next(self.c), end='')
-                        
+
     def stop(self):
         self.shouldstop=True
         print("\r")
-        
+
     def __enter__(self):
         threading._start_new_thread(self.start, ())
-        
+
     def __exit__(self, *args):
         self.stop()
 
-         
+
 
 
 if __name__ == "__main__":
